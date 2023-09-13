16913c2f08a64cfb40b06bd9ffd2218a1fa41004
Evelin <evelin4@tlu.ee>
2023-09-13 12:50:03 +0300

    basics.js

---
+++ lessons/13-09-2023/basics.js
@@ -0,0 +1,12 @@
+//Luua array numbritega ja leida spetsiifilise numbri index
+
+const array = [1,2,3,4, "hi"]
+
+function findMyIndex(array, element){
+    return array.indexOf(element)
+}
+
+console.log(findMyIndex(array, 4))
+
+//Funktsioon, mis liidab kaks numbrit kokku
+
